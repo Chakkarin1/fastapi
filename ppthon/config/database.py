bdc27f942b55e40ea4457c5edbb707b2670f2990
Chakkarin1 <89897308+Chakkarin1@users.noreply.github.com>
2021-11-23 22:36:04 +0700

    Update database.py

--- ppthon/config/database.py
+++ ppthon/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-# engine = create_engine("mysql+pymysql://root@localhost:3306/project")
-engine = create_engine("mysql+pymysql://project:oat41042@203.154.82.235:3306/project")
+engine = create_engine("mysql+pymysql://root@localhost:3306/project")
+# engine = create_engine("mysql+pymysql://project:oat41042@203.154.82.235:3306/project")
 meta = MetaData()
 
 conn = engine.connect()
